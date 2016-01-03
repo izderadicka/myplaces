772b93c1821ed0a380f38353081e5e03e0251911
izderadicka <ivan.zderadicka@gmail.com>
2016-01-03 11:02:07 +0100

    db vars

--- settings.py
+++ settings.py
@@ -34,11 +34,11 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': get_env('maps', 'DB_MAPS_NAME'),                      # Or path to database file if using sqlite3.
-        'USER': get_env('maps', 'DB_MAPS_USER'),                      # Not used with sqlite3.
-        'PASSWORD': get_env('maps', 'DB_MAPS_PASSWORD'),                  # Not used with sqlite3.
-        'HOST': get_env('127.0.0.1', 'DB_PORT_5432_TCP_ADDR'),                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT':  get_env('', 'DB_PORT_5432_TCP_PORT'),                     # Set to empty string for default. Not used with sqlite3.
+        'NAME': get_env('maps', 'DB_MAPS_NAME', 'PGDATABASE'),                      # Or path to database file if using sqlite3.
+        'USER': get_env('maps', 'DB_MAPS_USER', 'OPENSHIFT_POSTGRESQL_DB_USERNAME'),                      # Not used with sqlite3.
+        'PASSWORD': get_env('maps', 'DB_MAPS_PASSWORD', 'OPENSHIFT_POSTGRESQL_DB_PASSWORD'),                  # Not used with sqlite3.
+        'HOST': get_env('127.0.0.1', 'DB_PORT_5432_TCP_ADDR', 'OPENSHIFT_POSTGRESQL_DB_HOST'),                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT':  get_env('', 'DB_PORT_5432_TCP_PORT', 'OPENSHIFT_POSTGRESQL_DB_PORT'),                     # Set to empty string for default. Not used with sqlite3.
         
     }
 }
