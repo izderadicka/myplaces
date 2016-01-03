f60bf5b1b9684cec46352e869d5be7ce2f51639f
izderadicka <ivan.zderadicka@gmail.com>
2016-01-03 12:30:29 +0100

    build hook for static files

--- settings.py
+++ settings.py
@@ -83,7 +83,10 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/var/www/static'
+if 'OPENSHIFT_REPO_DIR' in os.environ:
+    STATIC_ROOT=os.path.join(curr_dir, 'wsgi', 'static')
+else:
+    STATIC_ROOT = '/var/www/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
