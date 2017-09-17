b5fc468af13c65f8276425f9366ca8a4d395093c
Ivan <ivan.zderadicka@gmail.com>
2017-09-17 10:10:33 +0200

    Starts in openshift but ws and zmq is not working

---
+++ deploy/openshift/app/main.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -ex
+
+DB_PASSWORD=${MAPS_DB_PASSWORD:-maps}
+DB_USER=${MAPS_DB_USER:-maps}
+DB_NAME=${MAPS_DB_NAME:-maps}
+DB_HOST=${MAPS_DB_HOST:-localhost}
+DB_PORT=${MAPS_DB_PORT:-5432}
+
+#TEST CONNECTION
+ADMIN_EXISTS=$(python manage.py shell<<EOF
+from django.contrib.auth.models import User
+User.objects.get(username='admin'); print 'ADMIN_EXISTS'
+EOF
+)
+
+init() {
+  echo "Initializing"
+  
+  # Create a PostgreSQL role named ``maps``  and
+  # then create a database `maps` owned by the ``maps`` role.
+  
+  python setup.py build_ext --inplace
+  python manage.py migrate
+  python manage.py loaddata myplaces/fixtures/default_groups.json
+  python manage.py shell <<EOF
+from django.contrib.auth.models import User
+User.objects.create_superuser('admin', 'admin@example.com', '${MAPS_ADMIN_PASSWORD:-admin}')
+EOF
+}
+
+if echo $ADMIN_EXISTS|grep ADMIN_EXISTS; then
+  echo "App Already initiallized"
+  
+else
+  echo "App Need initialization"
+  init
+fi
+
+#main entry point
+python runsocketio.py &
+python manage.py process_server &
+python manage.py runserver 0.0.0.0:8000
